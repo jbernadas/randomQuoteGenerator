498f1cb0ad9718e3f68a186f87eae9cfd7c1a262
jbernadas <jb3rns@gmail.com>
2019-05-12 19:55:15 -0500

    Added optional code for citation and year

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 /*** 
-  This is the source of quotes the generator retrieves from. Use the following format to add more quotes: 
+  This is the source of quotes the generator retrieves from. In using the following format to add more quotes, the last two items are optional: 
   {'quote': '', 'source': '', 'citation': '', 'year': }
 ***/
 var quotes = [
-  {'quote': 'Every artist was first an amateur.', 'source': 'Ralph Waldo Emerson', 'citation': 'Twisted Sifter', 'year': 2012},
-  {'quote': 'Creativity takes courage.', 'source': 'Henri Matisse', 'citation': 'Twisted Sifter', 'year': 2012},
-  {'quote': 'Every child is an artist. The problem is how to remain an artist once we grow up.', 'source': 'Pablo Picasso', 'citation': 'Twisted Sifter', 'year': 2012},
+  {'quote': 'Every artist was first an amateur.', 'source': 'Ralph Waldo Emerson'},
+  {'quote': 'Creativity takes courage.', 'source': 'Henri Matisse'},
+  {'quote': 'Every child is an artist. The problem is how to remain an artist once we grow up.', 'source': 'Pablo Picasso'},
   {'quote': 'You don’t take a photograph, you make it.', 'source': 'Ansel Adams', 'citation': 'Twisted Sifter', 'year': 2012},
-  {'quote': 'Art enables us to find ourselves and lose ourselves at the same time.', 'source': 'Thomas Merton', 'citation': 'Twisted Sifter', 'year': 2012},
+  {'quote': 'Art enables us to find ourselves and lose ourselves at the same time.', 'source': 'Thomas Merton'},
   {'quote': 'We don’t make mistakes, just happy little accidents.', 'source': 'Bob Ross', 'citation': 'Twisted Sifter', 'year': 2012},
   {'quote': 'A picture is a poem without words.', 'source': 'Horace', 'citation': 'Twisted Sifter', 'year': 2012},
   {'quote': 'The principles of true art is not to portray, but to evoke.', 'source': 'Jerzy Kosinski',  'citation': 'Twisted Sifter', 'year': 2012},
@@ -29,7 +29,7 @@ function getRandomQuote() {
 function printQuote() {
   var x = getRandomQuote();
   var outputQuote = document.getElementById('quote-box');
-  outputQuote.innerHTML = '<p class="quote">' + quotes[x]['quote'] + '</p><p class="source">' + quotes[x]['source'] + '<span class="citation">' + quotes[x]['citation'] + '</span><span class="year"> ' + quotes[x]['year'] + '</p>';
+  outputQuote.innerHTML = '<p class="quote">' + quotes[x]['quote'] + '</p><p class="source">' + quotes[x]['source'] + (quotes[x]['citation'] ? '<span class="citation">' + quotes[x]['citation'] + '</span>' : '') + (quotes[x]['year'] ? '<span class="year"> ' + quotes[x]['year'] + '</span>' : '') + '</p>';
 }
 
 // Invokes printQuote when the frontend button is clicked
